63741bafaa72754f775b147cdc41355304b341a9
William Ford <william@saphewebdesign.com>
2021-08-30 16:22:45 +0200

    Fix schema

--- src/graphql/Event/seed.ts
+++ src/graphql/Event/seed.ts
@@ -5,15 +5,24 @@ export default async function seedEvents(prisma: PrismaClient): Promise<void> {
     data: [
       {
         id: '87db2cfc-d4a9-406f-91ce-b73c1ee58c49',
+        teamId: '068eec80-9864-4e07-a0ba-e1a4cbb3902d',
         name: 'WDC 2021',
         slug: 'wdc-2021',
-        date: '',
+        date: '2021-08-01T00:00:00.000Z',
       },
       {
         id: '7794c798-60e5-466f-8332-0853cbb41c1f',
+        teamId: '068eec80-9864-4e07-a0ba-e1a4cbb3902d',
         name: 'DDT 2019',
         slug: 'ddt-2019',
-        date: '',
+        date: '2019-08-25T00:00:00.000Z',
+      },
+      {
+        id: '7b72e813-7aed-49ae-9d92-93c6f3797583',
+        teamId: '068eec80-9864-4e07-a0ba-e1a4cbb3902d',
+        name: 'VEXX',
+        slug: 'vexx',
+        date: '2020-08-07T00:00:00.000Z',
       },
     ],
   });
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,38 +1,24 @@
 import NexusPrismaScalars from 'nexus-prisma/scalars';
 import { makeSchema, fieldAuthorizePlugin, queryComplexityPlugin } from 'nexus';
 import path from 'path';
+import * as types from './types';
 import { FieldAuthorizePluginErrorConfig } from 'nexus/dist/plugins/fieldAuthorizePlugin';
-import glob from 'fast-glob';
 
-async function generateNexusSchema(): Promise<void> {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const types: any[] = [];
-  const cwd = './src/graphql/';
-  const paths = await glob(`${cwd}*/model.ts`);
-
-  for (const path of paths) {
-    const data = await import(`./${path.substr(cwd.length)}`);
-    types.push(data);
-  }
-
-  makeSchema({
-    types: [types, NexusPrismaScalars],
-    plugins: [
-      fieldAuthorizePlugin({
-        formatError: ({ error }: FieldAuthorizePluginErrorConfig): Error =>
-          error ?? new Error('Not authorized'),
-      }),
-      queryComplexityPlugin(),
-    ],
-    nonNullDefaults: {
-      input: true,
-      output: true,
-    },
-    outputs: {
-      typegen: path.join(__dirname, '/__generated__/nexus.d.ts'),
-      schema: path.join(__dirname, '../../schema.graphql'),
-    },
-  });
-}
-
-generateNexusSchema();
+export const schema = makeSchema({
+  types: [types, NexusPrismaScalars],
+  plugins: [
+    fieldAuthorizePlugin({
+      formatError: ({ error }: FieldAuthorizePluginErrorConfig): Error =>
+        error ?? new Error('Not authorized'),
+    }),
+    queryComplexityPlugin(),
+  ],
+  nonNullDefaults: {
+    input: true,
+    output: true,
+  },
+  outputs: {
+    typegen: path.join(__dirname, '/__generated__/nexus.d.ts'),
+    schema: path.join(__dirname, '../../schema.graphql'),
+  },
+});
