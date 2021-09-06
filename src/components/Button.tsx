8236b4061363a03aba01edfabd7c855e5e25bd33
saphewilliam <william@saphewebdesign.com>
2021-09-06 14:38:44 +0200

    404 page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,38 @@
 import cx from 'clsx';
+import Link from 'next/link';
 import React, { ReactElement } from 'react';
 
-export interface Props {
+interface PropsBase {
   label: string;
 }
 
+interface ButtonProps extends PropsBase {
+  onClick: () => void;
+}
+
+interface LinkProps extends PropsBase {
+  href: string;
+}
+
+export type Props = PropsBase | ButtonProps | LinkProps;
+
 export default function Button(props: Props): ReactElement {
-  return (
-    <button
-      className={cx(
-        'mt-2',
-        'bg-primary',
-        'text-white',
-        'w-full',
-        'py-3',
-        'rounded-lg',
-        'font-bold',
-      )}
-    >
-      {props.label}
-    </button>
+  const className = cx(
+    'block',
+    'mt-2',
+    'bg-primary',
+    'text-white',
+    'w-full',
+    'py-3',
+    'rounded-lg',
+    'font-bold',
+  );
+
+  return 'href' in props ? (
+    <Link href={props.href}>
+      <a className={className}>{props.label}</a>
+    </Link>
+  ) : (
+    <button className={className}>{props.label}</button>
   );
 }
