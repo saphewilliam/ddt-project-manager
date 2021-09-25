5c115c9f93779ef17816f63e55bb138f4fef9ca0
saphewilliam <william@saphewebdesign.com>
2021-09-25 22:52:43 +0200

    Fix git glitch

--- src/components/StoneList/StoneListColumnModal.tsx
+++ src/components/StoneList/StoneListColumnModal.tsx
@@ -12,24 +12,29 @@ export interface Props {
 
 export default function StoneListColumnModal(props: Props): ReactElement {
   return (
-    <Modal show={props.show} setShow={props.setShow} title="Show / Hide Columns" body={
-      <div className={cx('flex', 'flex-col')}>
-        {props.originalHeaders
-          .filter((header) => header.toggleHide)
-          .map((header, i) => (
-            <div key={i} className={cx('flex', 'items-center', 'space-x-3')}>
-              <input
-                tabIndex={props.show ? 0 : -1}
-                className={cx('text-primary', 'focus:ring-primary')}
-                type="checkbox"
-                id={`${props.title}-${header.name}`}
-                checked={!header.hidden}
-                onChange={() => header.toggleHide!()}
-              />
-              <label htmlFor={`${props.title}-${header.name}`}>{header.label}</label>
-            </div>
-          ))}
-      </div>
-    }/>
+    <Modal
+      show={props.show}
+      setShow={props.setShow}
+      title="Show / Hide Columns"
+      body={
+        <div className={cx('flex', 'flex-col')}>
+          {props.originalHeaders
+            .filter((header) => header.toggleHide)
+            .map((header, i) => (
+              <div key={i} className={cx('flex', 'items-center', 'space-x-3')}>
+                <input
+                  tabIndex={props.show ? 0 : -1}
+                  className={cx('text-primary', 'focus:ring-primary')}
+                  type="checkbox"
+                  id={`${props.title}-${header.name}`}
+                  checked={!header.hidden}
+                  onChange={() => header.toggleHide!()}
+                />
+                <label htmlFor={`${props.title}-${header.name}`}>{header.label}</label>
+              </div>
+            ))}
+        </div>
+      }
+    />
   );
 }
