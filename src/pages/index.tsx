52b12bdebb284e8cf4087bc85d772536c24a10f4
saphewilliam <william@saphewebdesign.com>
2021-11-24 19:03:01 +0100

    Fix broken home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
 import React, { ReactElement } from 'react';
 import Layout from '@components/Layout';
-import useWasm from '@hooks/useWasm';
 
 export default function HomePage(): ReactElement {
-  const { instance, loaded, error } = useWasm();
-
-  const title: string =
-    loaded && instance
-      ? instance.exports.addString('Hello', 'world') + (error ? error.message : '')
-      : '';
-
-  return <Layout title={title}></Layout>;
+  return <Layout title="Dashboard"></Layout>;
 }
