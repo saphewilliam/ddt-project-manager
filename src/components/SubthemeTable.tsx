5af92e871bd7e64403d5355a2ab89d7bc7845f78
basveenhoven <bas_veenhoven@hotmail.com>
2022-02-21 20:40:29 +0100

    Events overview

--- src/components/SubthemeTable.tsx
+++ src/components/SubthemeTable.tsx
@@ -1,5 +1,6 @@
-import { PlusSmIcon, MinusSmIcon } from '@heroicons/react/outline';
+import { PlusSmIcon, MinusSmIcon, PencilIcon } from '@heroicons/react/outline';
 import cx from 'clsx';
+import Link from 'next/link';
 import React, { ReactElement, useState } from 'react';
 
 export interface Props {
@@ -20,10 +21,17 @@ export default function SubthemeTable(props: Props): ReactElement {
   const [open, setOpen] = useState(false);
 
   return (
-    <table className={cx('table-auto', 'w-100')}>
+    <table className={cx('table-auto', 'w-full')}>
       <thead>
         <tr>
-          <td colSpan={3} style={{ backgroundColor: props.subtheme.color }}>
+          <td
+            colSpan={3}
+            style={{
+              backgroundColor: props.subtheme.color,
+              padding: '0.75rem 2rem',
+            }}
+            className={cx('align-middle')}
+          >
             <button
               className={cx('py-1', 'px-1', 'rounded', 'transition-colors')}
               onClick={() => setOpen(!open)}
@@ -45,14 +53,26 @@ export default function SubthemeTable(props: Props): ReactElement {
                   backgroundColor: props.subtheme.color,
                 }}
               ></td>
-              <td>
-                <span>{project.name}</span>
-                <span>{project.type}</span>
-                <span> 123456 stenen</span>
-                <span>Supervisor: {project.supervisor?.displayName}</span>
-                <span>{project.status}</span>
+              <td
+                className={cx('border-t-solid', 'border', 'border-gray-900/20')}
+                style={{
+                  padding: '0.75rem 2rem',
+                  fontSize: '14px',
+                }}
+              >
+                <span className={cx('flex')}>{project.name}</span>
+
+                <span className={cx('flex')}>{project.type}</span>
+                <span className={cx('flex')}> 123456 stenen</span>
+                <span className={cx('flex')}>Supervisor: {project.supervisor?.displayName}</span>
+                <span className={cx('flex')}>{project.status}</span>
               </td>
-              <td style={{ width: '90px' }}>Edit.....</td>
+              <td
+                className={cx('border-t-solid', 'border', 'border-gray-900/20')}
+                style={{ width: '90px' }}
+              >
+                <PencilIcon className={cx('flex', 'w-4')} />
+              </td>
             </tr>
           ))}
       </tbody>
--- src/pages/events/[eventSlug]/index.tsx
+++ src/pages/events/[eventSlug]/index.tsx
@@ -11,27 +11,34 @@ import { extractURLParam } from '@lib/util';
 export default function EventPage(): ReactElement {
   const router = useRouter();
 
-  const { data } = useSafeQuery('useEvent', {
-    eventSlug: extractURLParam('eventSlug', router.query) ?? '',
-  });
+  const eventSlug = extractURLParam('eventSlug', router.query);
+
+  const { data } = useSafeQuery(
+    'useEvent',
+    {
+      eventSlug: eventSlug ?? '',
+    },
+    eventSlug,
+  );
 
   return (
-    <Layout title="Event">
+    <Layout title="Event" hideHeader>
       <div className={cx('grid', 'md:grid-cols-2', 'gap-4', 'sm:grid-cols-none')}>
         <section
           className={cx(
             'flex',
             'justify-between',
             'col-span-2',
-            'border-solid',
-            'border',
+
             'rounded-xl',
             'transition-all',
+            'border-solid',
+            'border',
             'border-gray-900/20',
             'p-6',
           )}
         >
-          <h1 className={cx('font-bold', 'text-4xl')}>Projecten van {data?.event?.name} </h1>
+          <h2 className={cx('font-bold', 'text-3xl')}>Projecten van {data?.event?.name} </h2>
           <span className={cx()}>
             <Button
               className={cx('flex')}
@@ -121,9 +128,38 @@ export default function EventPage(): ReactElement {
             'p-6',
           )}
         >
-          <div>
-            <p>[attributenlijst]</p>
-          </div>
+          <table className={cx('table-auto', 'w-full')}>
+            <tbody>
+              <tr>
+                <th>Muizenvallen</th>
+                <td>7</td>
+              </tr>
+              <tr>
+                <th>Muurhaakjes</th>
+                <td>7</td>
+              </tr>
+              <tr>
+                <th>Diodes/Vlakstarterhouders</th>
+                <td>100</td>
+              </tr>
+              <tr>
+                <th>Vlakstarters (8 stenen)</th>
+                <td>3</td>
+              </tr>
+              <tr>
+                <th>Vlakstarters (10 stenen)</th>
+                <td>9</td>
+              </tr>
+              <tr>
+                <th>Vlakstarters (11 stenen)</th>
+                <td>37</td>
+              </tr>
+              <tr>
+                <th>Vlakstarters (12 stenen)</th>
+                <td>1</td>
+              </tr>
+            </tbody>
+          </table>
         </section>
       </div>
     </Layout>
