ee1cb8c40a0688bfd8b792e3fcba242710ef31a0
saphewilliam <william@saphewebdesign.com>
2022-02-22 20:30:21 +0100

    Wall template

--- src/wasm/index.ts
+++ src/wasm/index.ts
@@ -52,6 +52,10 @@ export class Canvas {
   }
 }
 
+// 4.8 cm hoog = 6
+// 2.4 cm breed = 3
+// 0.8 cm diep = 1
+
 export class PixelGridLayer {
   stones: Array<Stone>;
 
@@ -65,38 +69,36 @@ export class PixelGridLayer {
           new Point(x * stoneSize.width, y * stoneSize.height),
           new Size(stoneSize.width, stoneSize.height),
           // TODO
-          new Color(23, 104, 47),
+          new Color(0, 162, 232),
         );
       }
     }
   }
 }
 
-// 4.8 cm hoog = 6
-// 2.4 cm breed = 3
-// 0.8 cm diep = 1
-
 export class WallLayer {
   stones: Array<Stone>;
 
   constructor(width: u32, height: u32) {
-    this.stones = new Array<Stone>(width);
+    this.stones = new Array<Stone>();
 
-    const initColor = new Color(23, 104, 47);
+    const initColor = new Color(0, 162, 232);
     for (let y: u32 = 0; y < height; y++) {
       for (let x: u32 = 0; x < width; x++) {
-        if (y % 2 === 0)
-          this.stones[width * y + x] = new Stone(
-            new Point(x * 5, y * 3),
-            new Size(1, 3),
-            initColor,
-          );
-        else if (y % 2 === 1)
-          this.stones[width * y + x] = new Stone(
-            new Point(x * 5, y * 3),
-            new Size(6, 3),
-            initColor,
-          );
+        // Short stone row
+        if ((y % 2 === 0 && height % 2 === 1) || (y % 2 === 1 && height % 2 === 0))
+          this.stones.push(new Stone(new Point(x * 5, y * 3), new Size(1, 3), initColor));
+        // Long stone row
+        else {
+          if (x === width - 1) continue;
+          if ((x % 2 === 0 && y % 4 < 2) || (x % 2 === 1 && y % 4 > 1))
+            this.stones.push(new Stone(new Point(x * 5, y * 3), new Size(6, 3), initColor));
+          else if (x === 0)
+            this.stones.push(new Stone(new Point(x * 5, y * 3), new Size(5, 3), initColor));
+          else if (x === width - 2)
+            this.stones.push(new Stone(new Point(x * 5 + 1, y * 3), new Size(5, 3), initColor));
+          else this.stones.push(new Stone(new Point(x * 5 + 1, y * 3), new Size(4, 3), initColor));
+        }
       }
     }
   }
